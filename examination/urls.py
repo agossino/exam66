36815bf9af55d2ceaa8580942c763962e69268d7
Giancarlo Ossino <gcossino@gmail.com>
2023-09-25 18:31:36 +0200

    basic authentication

--- examination/urls.py
+++ examination/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("questions/", views.view, name="view"),
+    path("mcquestions/", views.list_mc_questions, name="ls_mcquest"),
+    # for test purpose
+    path("mdn", views.mdn, name="mdn"),
 ]
--- examination/views.py
+++ examination/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import permission_required, login_required
 from django.views.generic import TemplateView
 from django.shortcuts import get_list_or_404, render
+
 
 from .models import MultichoiceQuestion
 
@@ -8,6 +10,12 @@ class HomeView(TemplateView):
     template_name = "examination/home.html"
 
 
-def view(request):
+@permission_required("examination.view_multichoicequestion")
+# @login_required(login_url="/accounts/login/")
+def list_mc_questions(request):
     questions = get_list_or_404(MultichoiceQuestion.objects.order_by("id"))
-    return render(request, "examination/view.html", {"questions": questions})
+    return render(request, "examination/ls_mcquest.html", {"questions": questions})
+
+
+def mdn(request):
+    return render(request, "examination/mdn_structure.html")
